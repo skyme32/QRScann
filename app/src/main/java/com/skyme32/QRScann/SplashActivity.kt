ef8903155061e76898d1519f94bc2ade113b757c
skyme32 <marcosmejiasg@gmail.com>
2020-09-29 20:01:58 +0200

    Add styles and Splash activity

---
+++ app/src/main/java/com/skyme32/QRScann/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.skyme32.QRScann
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashActivity : AppCompatActivity() {
+
+    private val TIME_OUT: Long = 1000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, TIME_OUT)
+    }
+}
