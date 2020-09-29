f5d9c0095c64a47f79c764e0b28514c944105bcc
skyme32 <marcosmejiasg@gmail.com>
2020-09-29 13:21:58 +0200

    Init commit

---
+++ app/src/main/java/com/skyme32/QRScann/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.skyme32.QRScann
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
