fc19aff755563ccd78be5ebfeca842807ea0670a
skyme32 <marcosmejiasg@gmail.com>
2020-10-01 15:26:57 +0200

    Add CameraActivity

--- app/src/main/java/com/skyme32/qrscann/MainActivity.kt
+++ app/src/main/java/com/skyme32/qrscann/MainActivity.kt
@@ -1,11 +1,53 @@
 package com.skyme32.QRScann
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val REQUEST_CAMERA_PERMISSION = 10
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fab_camera.setOnClickListener {
+            //Snackbar.make(it , R.string.aboutInfo, Snackbar.LENGTH_SHORT).show()
+
+            // Request camera permissions
+            if (isCameraPermissionGranted()) {
+
+            } else {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), REQUEST_CAMERA_PERMISSION)
+            }
+        }
+    }
+
+    private fun isCameraPermissionGranted(): Boolean {
+        val selfPermission =
+            ContextCompat.checkSelfPermission(baseContext, Manifest.permission.CAMERA)
+        return selfPermission == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == REQUEST_CAMERA_PERMISSION) {
+            if (isCameraPermissionGranted()) {
+
+            } else {
+                Toast.makeText(this, "Camera permission is required.", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
